49e7855dd43ddf283abfc9cbed82d28c9c5bf778
[GitLab Kareem Keshk] <[GitLab kkeshk2@huskers.unl.edu]>
2023-11-08 15:08:44 -0600

    Implemented Cart controller

--- Store_App/Controllers/CartController.cs
+++ Store_App/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Store_App.Models.Classes;
 
 
@@ -8,14 +9,14 @@ namespace Store_App.Controllers
     [Route("[controller]")]
     public class CartController : Controller
     {
-        [HttpGet("/api/cart/getonebasedonaccountid/{id}")]
-        public Cart GetOneBasedOnAccountId(int userAccountId)
+        [HttpGet("/api/cart/getonebasedonaccountid")]
+        public ActionResult<string> GetOneBasedOnAccountId(int userAccountId)
         {
             Cart cart = new Cart();
             Cart retrievedCart = cart.GetOneBasedOnAccountId(userAccountId);
             if (retrievedCart != null)
             {
-                return retrievedCart;
+                return JsonConvert.SerializeObject(retrievedCart);
             }
             else
             {
@@ -24,7 +25,7 @@ namespace Store_App.Controllers
                     Errors = new List<string> { "Product not found" },
                     Success = false
                 };
-                return notFoundCart;
+                return JsonConvert.SerializeObject(notFoundCart);
             }
         }
     }
