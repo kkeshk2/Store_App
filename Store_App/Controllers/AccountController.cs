2aa9269c0f4c50dce26f91266003c0706bb84648
aph2419 <98563454+aph2419@users.noreply.github.com>
2023-11-17 16:38:13 -0600

    Added log in functionality

--- Store_App/Controllers/AccountController.cs
+++ Store_App/Controllers/AccountController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Store_App.Helpers;
 using Store_App.Models.Classes;
-
+using System.Net;
+using Newtonsoft.Json;
 
 namespace Store_App.Controllers
 {
-    public class AccountController : Controller
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AccountController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
+        [HttpGet("login")]
+        public ActionResult<string> LogIn(string email, string password)
+        {            
+            try
+            {
+                Account account = Account.accessAccountByLogin(email, password);
+                return JWTHelper.GetToken(account.AccountId);
+            }
+            catch (InvalidOperationException)
+            {
+                return new StatusCodeResult(400);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(502);
+            }
         }
     }
 }
