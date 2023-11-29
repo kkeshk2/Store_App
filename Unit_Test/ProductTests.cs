2544e549116c029914fd464b623929d5f9085a42
[GitLab Kareem Keshk] <[GitLab kkeshk2@huskers.unl.edu]>
2023-11-29 11:19:34 -0600

    Added some Product Unit Tests

---
+++ Unit_Test/ProductTests.cs
@@ -0,0 +1,24 @@
+﻿using Store_App.Models.Classes;
+
+namespace Unit_Test;
+
+[TestClass]
+public class ProductTests
+{
+    [TestMethod]
+    public void testGetAll_isValid()
+    {
+        Product myProduct = new Product();
+        List<Product> productList = myProduct.GetAll();
+        Assert.IsNotNull(productList);
+    }
+
+    [TestMethod]
+    public void GetById()
+    {
+        int id = 1;
+        Product myProduct = new Product();
+        Product returnedProduct = myProduct.GetOne(1);
+        Assert.AreEqual(id, returnedProduct.ProductId);
+    }
+}
